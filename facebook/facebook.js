da5a3d3f2510f8925416624f67ab11728c2479aa
psullivan6 <psullivan6@gmail.com>
2012-10-15 14:32:06 -0700

    fb13

--- facebook/facebook.js
+++ facebook/facebook.js
@@ -6,32 +6,32 @@
  * <script src='http://connect.facebook.net/en_US/all.js'></script>
  */
 
-	$('.sZ-facebook').click(function(){
+$('.sZ-facebook').click(function(){
 
-		FB.init({appId: "FACEBOOK_APP_ID", status: true, cookie: true});
+	FB.init({appId: "FACEBOOK_APP_ID", status: true, cookie: true});
 
-		var me = $(this);	// JavaScript 201 - Use $(this) in a variable so that it doesn't request the same element multiple times
+	var me = $(this);	// JavaScript 201 - Use $(this) in a variable so that it doesn't request the same element multiple times
 
-		// calling the API ...
-		var obj = {
-			method: 'feed',
-			link: me.attr('data-link'),			// This is an attribute of the above link tag, edit its contents above
-			picture: me.attr('data-image'),		// This is an attribute of the above link tag, edit its contents above
-			name: me.attr('data-title'),			// This is an attribute of the above link tag, edit its contents above
-			caption: me.attr('data-caption'),		// This is an attribute of the above link tag, edit its contents above
-			description: me.attr('data-description')	// This is an attribute of the above link tag, edit its contents above
-		};
+	// calling the API ...
+	var obj = {
+		method: 'feed',
+		link: me.attr('data-link'),			// This is an attribute of the above link tag, edit its contents above
+		picture: me.attr('data-image'),		// This is an attribute of the above link tag, edit its contents above
+		name: me.attr('data-title'),			// This is an attribute of the above link tag, edit its contents above
+		caption: me.attr('data-caption'),		// This is an attribute of the above link tag, edit its contents above
+		description: me.attr('data-description')	// This is an attribute of the above link tag, edit its contents above
+	};
 
-		function callback(response) {
-			if (response && response.post_id) {
-				// It was successfully posted, so do something here
-			} else {
-				// It failed, so do something else here
-			}
+	function callback(response) {
+		if (response && response.post_id) {
+			// It was successfully posted, so do something here
+		} else {
+			// It failed, so do something else here
 		}
+	}
 
-		FB.ui(obj, callback);
+	FB.ui(obj, callback);
 
-		return false;	// JavaScript 201 - Return false so that the link tag doesn't execute its native href attribute
+	return false;	// JavaScript 201 - Return false so that the link tag doesn't execute its native href attribute
 
-	});
+});
